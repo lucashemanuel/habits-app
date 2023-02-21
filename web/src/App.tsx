0e90cfa4a8ce3046dc361102c8d911c96cefcd7c
Lucas Hemanuel <lucashemanuel890@gmail.com>
2023-02-21 16:15:55 -0300

    added new server routes and finish frontend template

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,14 @@
-import { Habits } from "./components/Habits";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
-export default function App() {
+export function App() {
   return (
-    <div>
-      <Habits completed={3} />
-      <Habits completed={4} />
-      <Habits completed={9} />
-      <Habits completed={150} />
-      <Habits completed={143} />
-      <Habits completed={10} />
-      <Habits completed={33} />
-      <Habits completed={53} />
-      <Habits completed={38} />
-      <Habits completed={37} />
-      <Habits completed={30} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
